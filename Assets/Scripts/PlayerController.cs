08d33fbbc895d290c8bda4a5ab17ff3c61f8da08
Sylvie HOAREAU <hoareausms@gmail.com>
2025-09-23 10:55:13 +0400

    fix: corrections des bugs des colliders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,8 +9,8 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 moveInput;
 
-    [SerializeField] private float speed = 10;
-    [SerializeField] private float acceleration = 10;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float acceleration = 10f;
 
     // Appeler en premier avant meme le Start()
     void Awake()
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>(); // Récupère le composant Animator
 
+        // Toujours désactiver la gravité pour un mouvement cinématique
         rb.gravityScale = 0;
 
         // Crée un nouveau Player actions, pour pourvoir récuperer les inputs du joueurs
@@ -31,16 +32,17 @@ public class PlayerController : MonoBehaviour
     void LinkActions()
     {
         // Lie les actions du player input au fonctions correspondantes 
-        inputActions.PlayerInput.Move.started += OnMove; //started correspond a quand le joueur apuis sur le bouton
-        inputActions.PlayerInput.Move.performed += OnMove; //performed correspond a quand la valeur change (surtout utile avec les input de joystick)
-        inputActions.PlayerInput.Move.canceled += OnMove; //canceled correspond a quand le joueur relache le bouton
+        // inputActions.PlayerInput.Move.started += OnMove; //started correspond a quand le joueur apuis sur le bouton
+        inputActions.PlayerInput.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>(); //performed correspond a quand la valeur change (surtout utile avec les input de joystick)
+        inputActions.PlayerInput.Move.canceled += ctx => moveInput = Vector2.zero; //canceled correspond a quand le joueur relache le bouton
     }
 
     // Fixed update est appelé a chaque update du moteur physique. Il est preférable de modifier a ce moment les positions des objects physiques
     void FixedUpdate()
     {
         // Calcule de la nouvelle vélocité en fonction de input du joueur
-        Vector2 newVelocity = Vector2.Lerp(rb.linearVelocity, moveInput * speed, Time.fixedDeltaTime * acceleration);
+        Vector2 targetVelocity = moveInput * speed;
+        Vector2 newVelocity = Vector2.Lerp(rb.linearVelocity, targetVelocity, Time.fixedDeltaTime * acceleration);
         rb.linearVelocity = newVelocity;
     }
 
@@ -50,14 +52,14 @@ public class PlayerController : MonoBehaviour
         // et sont donc plus fluides si elles sont exécutées à chaque frame.
 
         // Gère la direction du sprite en fonction de l'input
-        if (moveInput.x > 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (moveInput.x < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
+        // if (moveInput.x > 0)
+        // {
+        //     transform.localScale = new Vector3(1, 1, 1);
+        // }
+        // else if (moveInput.x < 0)
+        // {
+        //     transform.localScale = new Vector3(-1, 1, 1);
+        // }
         
         // Met à jour la vitesse de l'Animator pour la transition d'Idle à Move
         animator.SetFloat("MoveSpeed", moveInput.magnitude);
@@ -68,16 +70,16 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("Vertical", moveInput.y);
     }
 
-    void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
+    // void OnMove(InputAction.CallbackContext context)
+    // {
+    //     moveInput = context.ReadValue<Vector2>();
 
-         // Si le mouvement est enclenché
-        if (moveInput != Vector2.zero)
-        {
-            // Mettez à jour les paramètres de direction de l'Animator
-            animator.SetFloat("Horizontal", moveInput.x);
-            animator.SetFloat("Vertical", moveInput.y);
-        }
-    }
+    //      // Si le mouvement est enclenché
+    //     if (moveInput != Vector2.zero)
+    //     {
+    //         // Mettez à jour les paramètres de direction de l'Animator
+    //         animator.SetFloat("Horizontal", moveInput.x);
+    //         animator.SetFloat("Vertical", moveInput.y);
+    //     }
+    // }
 }
